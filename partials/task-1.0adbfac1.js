0a020cdee5a74a1b9627ba4bbadb0dab89f2f6de
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2023-04-22 13:25:48 +0000

    Deploying to gh-pages from @ Aleksey-Dr/tasksJS@ff8d2a35c70c450b6326855c10eaa6506201a0ff 🚀

---
+++ partials/task-1.0adbfac1.js
@@ -0,0 +1,2 @@
+!function(){for(var o=function(o){setTimeout((function(){console.log(o)}),1)},n=function(o){setTimeout((function(){console.log(o)}),1)},t=0;t<3;t++)o(t);for(var c=0;c<3;c++)n(c)}();
+//# sourceMappingURL=task-1.0adbfac1.js.map
---
+++ partials/task-1.ab9e7254.js
@@ -0,0 +1,2 @@
+for(var o=0;o<3;o++)setTimeout((()=>{console.log(o)}),1);for(let o=0;o<3;o++)setTimeout((()=>{console.log(o)}),1);
+//# sourceMappingURL=task-1.ab9e7254.js.map
